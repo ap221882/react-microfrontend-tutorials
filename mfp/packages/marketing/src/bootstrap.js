9ad5b0d8a2b3142168f509819adf86f1b150ae73
ajayp <ajayp@naehas.com>
2022-04-26 12:54:36 +0530

    made marketing app

---
+++ mfp/packages/marketing/src/bootstrap.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<h1>Hi there!</h1>, el);
+};
+
+// If we r in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.getElementById('_marketing-dev-root');
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// If we r running through container, we export mount
+export { mount };
